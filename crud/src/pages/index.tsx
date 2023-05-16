6dd8365f8541dc375ebf0b7ba058a50ee3e05929
Iagohcc <iago.hcc@gmail.com>
2023-05-16 20:17:16 -0300

    criando componentes e classe cliente

--- crud/src/pages/index.tsx
+++ crud/src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
     <div className={`
-    flex h-screen justify-center items-center`}>
-      <span>Texto</span>
+    flex h-screen justify-center items-center
+    bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
+      <Layout titulo="Cadastro">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
